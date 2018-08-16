eacb8821d326f59b9eb478e7826e272119c549d0
kevinptx <kevin.peery@gmail.com>
2018-08-16 13:05:07 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Song.delete_all
+
+Song.create(title: "The Host of Seraphim", released: true, release_year: 1998, artist_name: "Lisa Gerrard", genre: "Dark wave, New-age music, Ethereal wave, Gothic rock")
+Song.create(title: "Kiko", released: true, release_year: 2012, artist_name: "Lisa Gerrard", genre: "Dark wave, New-age music, Ethereal wave, Gothic rock")
+Song.create(title: "Elysium", released: true, release_year: 2000, artist_name: "Lisa Gerrard", genre: "Dark wave, New-age music, Ethereal wave, Gothic rock")
+Song.create(title: "Sorrow", released: true, release_year: 2000, artist_name: "Lisa Gerrard", genre: "Dark wave, New-age music, Ethereal wave, Gothic rock")
+Song.create(title: "Violina", released: true, release_year: 1995, artist_name: "Lisa Gerrard", genre: "Dark wave, New-age music, Ethereal wave, Gothic rock")
